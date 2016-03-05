a8704667ca2201d13f49c5c8004dca4cfd3b0086
Duncan Beaton <dunckr@gmail.com>
2016-03-05 12:50:02 +0000

    add simplistic logic; add tests

--- index.js
+++ index.js
@@ -1,418 +1,60 @@
-/**
- * @author Duncan Beaton
- * @copyright 2014-2015 Duncan Beaton
- * @license MIT
- * @module retext:keywords
- * @fileoverview Keyword extraction with Retext.
- */
-
 'use strict';
 
-/*
- * Dependencies.
- */
-
-var stemmer = require('stemmer');
+var keys = require('object-keys');
 var visit = require('unist-util-visit');
+var difference = require('array-differ');
 var nlcstToString = require('nlcst-to-string');
-var pos = require('retext-pos');
+var quotation = require('quotation');
+var search = require('nlcst-search');
+var normalize = require('nlcst-normalize');
 
-/**
- * Get the stem of a node.
- *
- * @param {Node} node - Node to stem.
- * @return {string} - Stemmed node.
- */
-function stemNode(node) {
-  return stemmer(nlcstToString(node)).toLowerCase();
-}
+var stopwords = require('stopwords').english;
+var thesaurus = require('thesaurus/lib/th_en_US_new');
 
-/**
- * Check whether `value` is upper-case.
- *
- * @param {string} value - Value to check.
- * @return {boolean} - Whether `value` is upper-case.
- */
-function isUpperCase(value) {
-  return value === String(value).toUpperCase();
-}
+function attacher(processor, options) {
+  var ignore = (options || {}).ignore || stopwords;
+  var patterns = (options || {}).list || thesaurus;
+  var limit = (options || {}).limit || 3;
+  var list = keys(patterns);
+  var phrases = difference(list, ignore);
 
-/**
- * Reverse sort: from 9 to 0.
- *
- * @param {number} a - First.
- * @param {number} b - Second.
- * @return {number} - Difference.
- */
-function reverse(a, b) {
-  return b - a;
-}
+  function transformer(tree, file) {
+    var duplicates = {}
+    search(tree, phrases, function(match, position, parent, phrase) {
+      duplicates[phrase] = duplicates[phrase] ? duplicates[phrase] + 1 : 1
+    });
 
-/**
- * Check whether or not a `node` is important.
- *
- * @param {Node} node - Node to check.
- * @return {boolean} - Whether `node` is important.
- */
-function isImportant(node) {
-  return (
-    node &&
-    node.data &&
-    node.data.partOfSpeech &&
-    (
-      node.data.partOfSpeech.indexOf('N') === 0 ||
-      (
-        node.data.partOfSpeech === 'JJ' &&
-        isUpperCase(nlcstToString(node).charAt(0))
-      )
-    )
-  );
-}
 
-/**
- * Get most important words in `node`.
- *
- * @param {Node} node - Parent to search in.
- * @return {Array.<Object>}
- */
-function getImportantWords(node) {
-  var words = {};
-
-  visit(node, 'WordNode', function(word, index, parent) {
-    var match;
-    var stem;
-
-    if (isImportant(word)) {
-      stem = stemNode(word);
-      match = {
-        'node': word,
-        'index': index,
-        'parent': parent
-      };
-
-      if (!words[stem]) {
-        words[stem] = {
-          'matches': [match],
-          'stem': stem,
-          'score': 1
-        };
-      } else {
-        words[stem].matches.push(match);
-        words[stem].score++;
+    var duplicate;
+    for (duplicate in duplicates) {
+      if (duplicates[duplicate] < limit) {
+        delete duplicates[duplicate]
       }
     }
-  });
 
-  return words;
-}
+    var duplicateKeys = keys(duplicates);
 
-/**
- * Get the top results from an occurance map.
- *
- * @param {Object.<string, Object>} results - Map of stems
- *   mapping to objects containing `nodes`, `stem`, and
- *   `score` properties.
- * @param {number} maximum - Try to get at least `maximum`
- *   results.
- * @return {Array.<Object>}
- */
-function filterResults(results, maximum) {
-  var filteredResults = [];
-  var indices = [];
-  var matrix = {};
-  var column;
-  var key;
-  var score;
-  var interpolated;
-  var index;
-  var otherIndex;
-  var maxScore;
+    search(tree, duplicateKeys, function(match, position, parent, phrase) {
 
-  for (key in results) {
-    score = results[key].score;
+      var suggestions = patterns[phrase];
+      var value = quotation(nlcstToString(match), '“', '”');
 
-    if (!matrix[score]) {
-      matrix[score] = [];
-      indices.push(score);
-    }
+      var reason = 'Replace ' + value + ' with ' +
+      quotation(suggestions, '“', '”').join(', ');
 
-    matrix[score].push(results[key]);
-  }
+      var message = file.warn(reason, {
+        'start': match[0].position.start,
+        'end': match[match.length - 1].position.end
+      });
 
-  indices.sort(reverse);
+      message.ruleId = phrase;
+      message.source = 'retext-overuse';
 
-  maxScore = indices[0];
+    });
 
-  index = -1;
-
-  while (indices[++index]) {
-    score = indices[index];
-    column = matrix[score];
-
-    interpolated = score / maxScore;
-    otherIndex = -1;
-
-    while (column[++otherIndex]) {
-      column[otherIndex].score = interpolated;
-    }
-
-    filteredResults = filteredResults.concat(column);
-
-    if (filteredResults.length >= maximum) {
-      break;
-    }
-  }
-
-  return filteredResults;
-}
-
-/**
- * Get following or preceding important words or white space.
- *
- * @param {Node} node - Node to start search at.
- * @param {number} index - Position of `node` in `parent`.
- * @param {Node} parent - Parent of `node`.
- * @param {number} offset - Offset to the next node. `-1`
- *   when iterating backwards, `1` when iterating forwards.
- * @return {Object}
- */
-function findPhraseInDirection(node, index, parent, offset) {
-  var children = parent.children;
-  var nodes = [];
-  var stems = [];
-  var words = [];
-  var queue = [];
-  var child;
-
-  while (children[index += offset]) {
-    child = children[index];
-
-    if (child.type === 'WhiteSpaceNode') {
-      queue.push(child);
-    } else if (isImportant(child)) {
-      nodes = nodes.concat(queue, [child]);
-      words.push(child);
-      stems.push(stemNode(child));
-      queue = [];
-    } else {
-      break;
-    }
-  }
-
-  return {
-    'stems': stems,
-    'words': words,
-    'nodes': nodes
-  };
-}
-
-/**
- * Merge a previous array, with a current value, and
- * a following array.
- *
- * @param {Array.<*>} prev - Reversed array before `current`.
- * @param {*} current - Current thing.
- * @param {Array.<*>} next - Things after `current`.
- * @return {Array.<*>}
- */
-function merge(prev, current, next) {
-  return prev.concat().reverse().concat([current], next);
-}
-
-/**
- * Find the phrase surrounding a node.
- *
- * @param {Object} match - Single match.
- * @return {Object}
- */
-function findPhrase(match) {
-  var node = match.node;
-  var prev = findPhraseInDirection(node, match.index, match.parent, -1);
-  var next = findPhraseInDirection(node, match.index, match.parent, 1);
-  var stems = merge(prev.stems, stemNode(node), next.stems);
-
-  return {
-    'stems': stems,
-    'value': stems.join(' '),
-    'nodes': merge(prev.nodes, node, next.nodes)
-  };
-}
-
-/**
- * Get the top important phrases in `self`.
- *
- * @param {Object.<string, Object>} results - Map of stems
- *   mapping to objects containing `nodes`, `stem`, and
- *   `score` properties.
- * @param {number} maximum - Try to get at least `maximum`
- *   results.
- * @return {Array.<Object>}
- */
-function getKeyphrases(results, maximum) {
-  var stemmedPhrases = {};
-  var initialWords = [];
-  var stemmedPhrase;
-  var index;
-  var length;
-  var otherIndex;
-  var keyword;
-  var matches;
-  var phrase;
-  var stems;
-  var score;
-  var first;
-  var match;
-
-  /*
-   * Iterate over all grouped important words...
-   */
-
-  for (keyword in results) {
-    matches = results[keyword].matches;
-    length = matches.length;
-    index = -1;
-
-    /*
-     * Iterate over every occurence of a certain keyword...
-     */
-
-    while (++index < length) {
-      phrase = findPhrase(matches[index]);
-      stemmedPhrase = stemmedPhrases[phrase.value];
-      first = phrase.nodes[0];
-
-      match = {
-        'nodes': phrase.nodes,
-        'parent': matches[index].parent
-      };
-
-      /*
-       * If we've detected the same stemmed
-       * phrase somewhere.
-       */
-
-      if (stemmedPhrase) {
-        /*
-         * Add weight per phrase to the score of
-         * the phrase.
-         */
-
-        stemmedPhrase.score += stemmedPhrase.weight;
-
-        /*
-         * If this is the first time we walk over
-         * the phrase (exact match but containing
-         * another important word), add it to the
-         * list of matching phrases.
-         */
-
-        if (initialWords.indexOf(first) === -1) {
-          initialWords.push(first);
-          stemmedPhrase.matches.push(match);
-        }
-      } else {
-        otherIndex = -1;
-        score = -1;
-        stems = phrase.stems;
-
-        initialWords.push(first);
-
-        /*
-         * For every stem in phrase, add its
-         * score to score.
-         */
-
-        while (stems[++otherIndex]) {
-          score += results[stems[otherIndex]].score;
-        }
-
-        stemmedPhrases[phrase.value] = {
-          'score': score,
-          'weight': score,
-          'stems': stems,
-          'value': phrase.value,
-          'matches': [match]
-        };
-      }
-    }
-  }
-
-  for (stemmedPhrase in stemmedPhrases) {
-    phrase = stemmedPhrases[stemmedPhrase];
-
-    /*
-     * Modify its score to be the rounded result of
-     * multiplying it with the number of occurances,
-     * and dividing it by the ammount of words in the
-     * phrase.
-     */
-
-    phrase.score = Math.round(
-      phrase.score * phrase.matches.length / phrase.stems.length
-    );
-  }
-
-  return filterResults(stemmedPhrases, maximum);
-}
-
-/**
- * Clone the given map of words.
- *
- * This is a two level-deep clone.
- *
- * @param {Object} words - Important words.
- * @return {Object} - Cloned words.
- */
-function cloneMatches(words) {
-  var result = {};
-  var key;
-  var match;
-
-  for (key in words) {
-    match = words[key];
-    result[key] = {
-      'matches': match.matches,
-      'stem': match.stem,
-      'score': match.score
-    }
-  }
-
-  return result;
-}
-
-/**
- * Attach.
- *
- * @param {Retext} retext - Instance.
- * @param {Object?} [options] - Configuration.
- * @param {number?} [options.maximum] - Try to get at
- *   least `maximum` results.
- * @return {Function} - `transformer`.
- */
-function attacher(retext, options) {
-  var maximum = (options || {}).maximum || 5;
-
-  retext.use(pos);
-
-  /**
-   * Attach keywords in `cst` to `file`.
-   *
-   * @param {NLCSTNode} cst - Node.
-   * @param {VFile} file - Virtual file.
-   */
-  function transformer(cst, file) {
-    var space = file.namespace('retext');
-    var important = getImportantWords(cst);
-
-    space.keywords = filterResults(cloneMatches(important), maximum);
-    space.keyphrases = getKeyphrases(important, maximum);
   }
 
   return transformer;
 }
-
-/*
- * Expose.
- */
 
 module.exports = attacher;
--- test.js
+++ test.js
@@ -1,152 +1,49 @@
+/**
+ * @author Duncan Beaton
+ * @copyright 2016 Duncan Beaton
+ * @license MIT
+ * @module retext:overuse
+ * @fileoverview Check phrases overuse.
+ */
+
 'use strict';
 
-/* eslint-env mocha */
+/* eslint-env node */
 
 /*
  * Dependencies.
  */
 
-var assert = require('assert');
+var test = require('tape');
 var retext = require('retext');
-var keywords = require('./');
-
-/*
- * Methods.
- */
-
-var equal = assert.strictEqual;
-
-/*
- * Fixture.
- *
- * First three paragraphs on Term Extraction from Wikipedia:
- * http://en.wikipedia.org/wiki/Terminology_extraction
- */
-
-var fixture = 'Terminology mining, term extraction, term recognition, or ' +
-    'glossary extraction, is a subtask of information extraction. ' +
-    'The goal of terminology extraction is to automatically extract ' +
-    'relevant terms from a given corpus.' +
-    '\n\n' +
-    'In the semantic web era, a growing number of communities and ' +
-    'networked enterprises started to access and interoperate through ' +
-    'the internet. Modeling these communities and their information ' +
-    'needs is important for several web applications, like ' +
-    'topic-driven web crawlers, web services, recommender systems, ' +
-    'etc. The development of terminology extraction is essential to ' +
-    'the language industry.' +
-    '\n\n' +
-    'One of the first steps to model the knowledge domain of a ' +
-    'virtual community is to collect a vocabulary of domain-relevant ' +
-    'terms, constituting the linguistic surface manifestation of ' +
-    'domain concepts. Several methods to automatically extract ' +
-    'technical terms from domain-specific document warehouses have ' +
-    'been described in the literature.' +
-    '\n\n' +
-    'Typically, approaches to automatic term extraction make use of ' +
-    'linguistic processors (part of speech tagging, phrase chunking) ' +
-    'to extract terminological candidates, i.e. syntactically ' +
-    'plausible terminological noun phrases, NPs (e.g. compounds ' +
-    '"credit card", adjective-NPs "local tourist information office", ' +
-    'and prepositional-NPs "board of directors" - in English, the ' +
-    'first two constructs are the most frequent). Terminological ' +
-    'entries are then filtered from the candidate list using ' +
-    'statistical and machine learning methods. Once filtered, ' +
-    'because of their low ambiguity and high specificity, these terms ' +
-    'are particularly useful for conceptualizing a knowledge domain ' +
-    'or for supporting the creation of a domain ontology. Furthermore, ' +
-    'terminology extraction is a very useful starting point for ' +
-    'semantic similarity, knowledge management, human translation ' +
-    'and machine translation, etc.';
-
-var keyScores = [1, 1, 0.71, 0.57, 0.57];
-var phraseScores = [1, 0.55, 0.53, 0.24, 0.18];
+var overuse = require('./');
 
 /*
  * Tests.
  */
 
-describe('pos()', function () {
-    retext().use(keywords).process(fixture, function (err, file) {
-        it('should not fail', function (done) {
-            done(err);
-        });
+test('retext-overuse', function(t) {
+  t.plan(2);
 
-        var namespace = file.namespace('retext');
+  retext()
+    .use(overuse)
+    .process([
+      'That movie was amazing',
+      'The acting was amazing',
+      'The story was amazing',
+    ].join('\n'), function(err, file) {
 
-        it('should work', function () {
-            assert('keywords' in namespace);
-            assert('keyphrases' in namespace);
+      t.ifError(err, 'should not fail (#1)');
 
-            equal(namespace.keywords.length, 5);
-            equal(namespace.keyphrases.length, 5);
-        });
+      t.deepEqual(
+        file.messages.map(String), [
+          '1:16-1:23: Replace “amazing” with “astonishing”, “surprising”, “awe-inspiring”, “awesome”, “awful”, “awing”, “impressive”', 
+          '2:16-2:23: Replace “amazing” with “astonishing”, “surprising”, “awe-inspiring”, “awesome”, “awful”, “awing”, “impressive”', 
+          '3:15-3:22: Replace “amazing” with “astonishing”, “surprising”, “awe-inspiring”, “awesome”, “awful”, “awing”, “impressive”'
+        ],
+        'should warn about the use of overused words'
+      );
 
-        it('should have scores', function () {
-            namespace.keywords.forEach(function (keyword, n) {
-                equal(Math.round(keyword.score * 1e2) / 1e2, keyScores[n]);
-            });
-
-            namespace.keyphrases.forEach(function (phrase, n) {
-                equal(Math.round(phrase.score * 1e2) / 1e2, phraseScores[n]);
-            });
-        });
-
-        it('should have stems', function () {
-            namespace.keywords.forEach(function (keyword) {
-                assert('stem' in keyword);
-            });
-
-            namespace.keyphrases.forEach(function (phrase) {
-                assert('stems' in phrase);
-            });
-        });
-
-        it('should have matches', function () {
-            namespace.keywords.forEach(function (keyword) {
-                assert('matches' in keyword);
-            });
-
-            namespace.keyphrases.forEach(function (phrase) {
-                assert('matches' in phrase);
-            });
-        });
-
-        describe('keywords[n].matches[n]', function () {
-            it('should have node, index, and parent', function () {
-                namespace.keywords.forEach(function (keyword) {
-                    keyword.matches.forEach(function (match) {
-                        assert('node' in match);
-                        assert('parent' in match);
-                        assert('index' in match);
-                    });
-                });
-            });
-        })
-
-        describe('keyphrases', function () {
-            it('should have a weight', function () {
-                namespace.keyphrases.forEach(function (phrase) {
-                    assert('weight' in phrase);
-                });
-            });
-
-            it('should have a value', function () {
-                namespace.keyphrases.forEach(function (phrase) {
-                    assert('value' in phrase);
-                });
-            });
-        })
-
-        describe('keyphrases[n].matches[n]', function () {
-            it('should have nodes and parent', function () {
-                namespace.keyphrases.forEach(function (phrase) {
-                    phrase.matches.forEach(function (match) {
-                        assert('nodes' in match);
-                        assert('parent' in match);
-                    });
-                });
-            });
-        })
     });
+
 });
