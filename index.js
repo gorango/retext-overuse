200f804191cecb9faefc854a979f2ed53ed1cb75
Duncan Beaton <dunckr@gmail.com>
2016-03-05 15:10:34 +0000

    fix linting

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ var thesaurus = require('thesaurus/lib/th_en_US_new');
  * Get any duplicated Words in `node`.
  *
  * @param {Node} node - Parent to search in.
- * @param {Array.<String>} phrases - Phrases to search for.
- * @return {Array.<Object>}
+ * @param {Array.<string>} phrases - Phrases to search for.
+ * @return {Object}
  */
 function getDuplicates(node, phrases) {
   var duplicates = {};
-  search(node, phrases, function(match, position, parent, phrase) {
+  search(node, phrases, function (match, position, parent, phrase) {
     duplicates[phrase] = duplicates[phrase] ? duplicates[phrase] + 1 : 1
   });
   return duplicates;
@@ -42,9 +42,9 @@ function getDuplicates(node, phrases) {
 /**
  * Get duplicates that are used too frequently.
  *
- * @param {Array.<Object>} phrases - Phrases to search for.
- * @param {Number} limit - Number of times phrase must have been used.
- * @return {Array.<String>}
+ * @param {Object} duplicates - Phrases to search for.
+ * @param {number} limit - Number of times phrase must have been used.
+ * @return {Array.<string>}
  */
 function getOveruse(duplicates, limit) {
   var duplicate;
@@ -64,11 +64,11 @@ function getOveruse(duplicates, limit) {
  * @param {Object.<Object>?} [options.list] - Phrases with suggestions
  * @param {Object.<Array>?} [options.ignore] - Phrases in the list
  * to ignore.
- * @param {Object.<Number>?} [options.limit] - Number of times phrase
+ * @param {Object.<number>?} [options.limit] - Number of times phrase
  * must have been used.
  * @return {Function} - `transformer`.
  */
-function attacher(processor, options) {
+function attacher(retext, options) {
   var settings = options || {};
   var ignore = settings.ignore || stopwords;
   var patterns = settings.list || thesaurus;
@@ -86,7 +86,7 @@ function attacher(processor, options) {
     var duplicates = getDuplicates(tree, phrases)
     var overuse = getOveruse(duplicates, limit)
 
-    search(tree, overuse, function(match, position, parent, phrase) {
+    search(tree, overuse, function (match, position, parent, phrase) {
       var suggestions = patterns[phrase];
       var value = quotation(nlcstToString(match), '“', '”');
 
